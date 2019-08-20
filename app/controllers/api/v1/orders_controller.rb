3308c20e2ee7e4ff92ed6a605072d64b79c9155a
sibhat <sibhat.temelso@gmail.com>
2019-08-20 14:40:14 -0700

    Extract Shipment Handling to Model

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -29,18 +29,7 @@ module Api
 
       def ship
         @order = Order.find(params[:id])
-        product_ids = OrderProduct.where(order_id: params[:id]).pluck(:product_id)
-        @product = Product.find(product_ids)
-
-        shippable = @product.length >= 1 && @order.status != "shipped"
-        
-        
-        unless shippable
-          render json: { message: "There was a problem shipping your order." }, status: 401 
-          return
-        end
-        
-        if @order.update(status: "shipped")
+        if @order.ship
           render json: @order, status: :ok, location: api_v1_order_url(@order)
         else
           render json: { message: "There was a problem shipping your order." }, status: :unprocessable_entity
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,22 @@
 class Order < ApplicationRecord
+    attr_reader :products, :order
+    def products
+        @product_ids = OrderProduct.where(order_id: id).pluck(:product_id)
+        @products = Product.find(@product_ids)
+    end
+    def shippable?
+        products
+        return products.length >= 1 && status != "shipped"
+    end
+    def ship
+        unless shippable?
+            return false
+        end
+        if update(status: "shipped")
+            return true
+        else
+            return false
+        end
+    end
+
 end
